336d26dee812457795f7d0b94147099cf5fdf86f
Sonia Hakobian <sonyahakobyan97@gmail.com>
2017-12-01 15:22:51 +0400

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 class List{
  public:
  struct Node{
@@ -14,7 +13,6 @@ class List{
   void add(int n);
   friend List sum(List l1, List l2);
 };
-
 List::List()
 {
   this->length=0;
@@ -24,7 +22,6 @@ int List::getlength()
 {
   return length;
 }
-
 void List::add(int n)
 {
   Node* node=new Node;
@@ -44,46 +41,62 @@ while(head!=nullptr)
 List sum(List l1, List l2)
 {
   List l3;
-  int tmp;
-  int dec=0;
+  int tmp = 0;
+  int dec = 0; 
   if(l1.getlength()<=l2.getlength())
   {
-    for(int i=0;i<=l2.getlength();i++)
+    for(int i=0;i<l2.getlength();i++)
     {
-      tmp=l1.head->data+l2.head->data;
+      if (i>=l1.getlength())
+      {
+          tmp=l2.head->data + dec ;
+      }
+      else 
+      tmp=l1.head->data+l2.head->data + dec ;
       if(tmp > 10)
       {
         tmp=tmp-10;
         l3.add(tmp);
-        l3.add(++dec);
         dec=0;
-        l1.head=l1.head->next;
+        dec++;
+        if (i<l1.getlength()){
+          l1.head=l1.head->next;
+        }
         l2.head=l2.head->next;
+        if(i == l2.getlength()-1 && dec > 0)
+        {
+         l3.add(dec);
+        }
       }
       else
       {
       l3.add(tmp);
+      dec = 0;
+      if (i<l1.getlength()){
+        l1.head=l1.head->next;
       }
-
+        l2.head=l2.head->next;
+      }
     }
   }
   return l3;
 }
-
 int main()
 {
   List L1;
   L1.add(2);
-  L1.add(2);
-  L1.add(2);
+  L1.add(5);
+  L1.add(7);
+  
 
   List L2;
+  L2.add(2);
   L2.add(3);
-  L2.add(3);
-  L2.add(3);
+  L2.add(9);
+  L2.add(1);
+  L2.add(5);
 
   sum(L1,L2).print();
-
-
   return 0;
+
 }
